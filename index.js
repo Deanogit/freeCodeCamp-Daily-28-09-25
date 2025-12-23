9908a7948ea8d913ab8bf58d8ba5649889664741
Dean Howe <hideanhowe@gmail.com>
2025-12-23 11:56:07 +0000

    solution

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 
 // The first line of a CSV file contains headings separated by commas.
 // Remove any leading or trailing whitespace from each heading.
+
+function getHeadings(csv) {
+  console.log(csv);
+  // split
+  const split = csv.split(',');
+  console.log(split);
+  const clean = [];
+  for (const x of split) {
+    clean.push(x.trim());
+  }
+  console.log(clean);
+  return clean;
+  //return split
+  //return csv;
+}
+
+getHeadings('username , email , signup date ');
